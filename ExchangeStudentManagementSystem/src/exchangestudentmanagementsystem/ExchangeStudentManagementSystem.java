c70cbb6a752f8da5df90839fcd84409306178f9f
Gorata Mannathoko <Gorata Mannathoko@GiGiMannasYd92.mshome.net>
2015-11-04 12:55:54 +0200

    added a useless if statement with an error

--- ExchangeStudentManagementSystem/src/exchangestudentmanagementsystem/ExchangeStudentManagementSystem.java
+++ ExchangeStudentManagementSystem/src/exchangestudentmanagementsystem/ExchangeStudentManagementSystem.java
@@ -16,6 +16,8 @@ public class ExchangeStudentManagementSystem {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        if ( Student == stud)
+            stud = stude;
     }
     
 }
