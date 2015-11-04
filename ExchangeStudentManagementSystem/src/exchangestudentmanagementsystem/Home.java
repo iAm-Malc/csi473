9af65f94a192c05d2736e72d798239f304059653
BT <BT@Brandon1-PC>
2015-11-04 21:16:18 +0200

    create application form

--- ExchangeStudentManagementSystem/src/exchangestudentmanagementsystem/Home.java
+++ ExchangeStudentManagementSystem/src/exchangestudentmanagementsystem/Home.java
@@ -95,6 +95,7 @@ public class Home extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void studentLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_studentLoginActionPerformed
